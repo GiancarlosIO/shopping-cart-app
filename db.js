8742cdbc3dec9c1fcae5cb2719a09c3d995de3dd
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-05-19 01:18:43 -0500

    Refactor data api

--- db.js
+++ db.js
@@ -5,9 +5,9 @@ module.exports = () => {
   for (let i=0; i < 100; i++) {
     data.products.push({
       id: i,
-      name: `Product Nº ${i}`,
-      image: 'http://lorempixel.com/100/150/animals/',
-      price: `$ ${i*10}.00`,
+      name: `Product Nº ${i + 1}`,
+      image: 'http://lorempixel.com/150/150/animals/',
+      price: `$ ${(i+1)*10}.00`,
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
     });
   };
