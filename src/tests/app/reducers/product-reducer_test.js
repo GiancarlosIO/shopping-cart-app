d782e32466bc522ed6840529bf850637a67e56c2
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-05-19 15:25:29 -0500

    Add unit tests for products rducer

---
+++ src/tests/app/reducers/product-reducer_test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { expect } from 'chai';
+import * as Constants from '../../../app/constants/';
+import ProductReducer from '../../../app/reducers/product-reducer';
+
+describe('Product Reducer', () => {
+  const initialState = {
+    all: null,
+    searchText: null
+  }
+  it('should return the initial state', () => {
+    expect(ProductReducer(undefined, {})).to.eql(initialState);
+  })
+  it('should handle SET_PRODUCTS', () => {
+    expect(ProductReducer(initialState, {
+      type: Constants.SET_PRODUCTS,
+      payload: { 1: { id: 1 } }
+    })).to.eql({
+      all: {
+        1: { id: 1 }
+      },
+      searchText: null
+    });
+  });
+  it('should handle SET_SEARCH_TEXT', () => {
+    expect(ProductReducer(initialState, {
+      type: Constants.SET_SEARCH_TEXT,
+      payload: 'search text'
+    })).to.eql({
+      all: null,
+      searchText: 'search text'
+    });
+  });
+});
