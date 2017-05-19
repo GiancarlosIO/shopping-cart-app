1ebc56ce62466179020817639121482fa4bec7aa
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-05-19 01:22:51 -0500

    Refactor styles for product-list component

---
+++ src/app/components/products/product-list.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import glamorous from 'glamorous';
+
+import Product from './product';
+
+const PaddedDiv = glamorous.div({
+  padding: '20px 0',
+  display: 'flex',
+  flexFlow: 'row wrap',
+  justifyContent: 'space-around'
+});
+
+const ProductList = ({ products, handleClick }) => {
+
+  const renderProducts = () => {
+    return Object.keys(products).length > 0 ?
+      Object.keys(products).map( index => (
+        <Product key={index} handleClick={handleClick} {...products[`${index}`]} />
+      )) :
+      (<h3>Products not found ヽ(ヅ)ノ</h3>)
+  }
+
+  return (
+    <PaddedDiv>
+      {
+        products ?
+        renderProducts() :
+        (<h3>Loading...</h3>)
+      }
+    </PaddedDiv>
+  );
+}
+
+export default ProductList;
