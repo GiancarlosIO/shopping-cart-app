d020ce6c415ae774765e901f5bf26dcc03f7fdca
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-05-19 16:15:55 -0500

    Add tests for filter component

--- src/app/components/products/filter.js
+++ src/app/components/products/filter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Filter = (props) => {
+export const Filter = (props) => {
 
   const handleChange = (e) => props.handleChange(e);
 
