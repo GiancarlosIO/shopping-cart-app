623131fd5dfc8c2488a2c3df0f7e08f74069a818
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-05-19 03:15:52 -0500

    Add cases to handle actions in cart-reducer

--- src/app/reducers/cart-reducer.js
+++ src/app/reducers/cart-reducer.js
@@ -1,10 +1,20 @@
 import {
   ADD_PRODUCT_CART,
-  REMOVE_PRODUCT_CART
+  REMOVE_PRODUCT_CART,
+  UPDATE_QUANTITY_CAR
 } from '../constants/';
 
 const initialState = {
-  products: {},
+  products: {
+    0 : {
+      id: 0,
+      name: "Product Nº 1",
+      price: "99999",
+      image: "http://lorempixel.com/150/150/animals/",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+      quantity: 3
+    }
+  },
   subtotal: null,
   total: null
 };
@@ -15,7 +25,7 @@ export default function(state=initialState, action) {
       let product = {
         ...action.payload,
         quantity: state.products[`${action.payload.id}`] ? state.products[`${action.payload.id}`].quantity + 1 : 1
-      }
+      };
       return {
         ...state,
         products: {
@@ -24,11 +34,22 @@ export default function(state=initialState, action) {
         }
       };
     case REMOVE_PRODUCT_CART:
+      console.log('removing', action);
       let products = { ...state.products };
-      delete products[`${action.payload.id}`];
+      delete products[`${action.payload}`];
       return {
         ...state,
         products
+      }
+    case UPDATE_QUANTITY_CAR:
+      let productToUpdate = { ...state.products[`${action.payload.id}`] };
+      productToUpdate.quantity = action.payload.quantity;
+      return {
+        ...state,
+        products: {
+          ...state.products,
+          [`${productToUpdate.id}`]: productToUpdate
+        }
       }
     default:
       return state;
