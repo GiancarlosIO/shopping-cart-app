39023aa9912c2e85dc0494e4e29802500d63453f
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-05-18 22:34:49 -0500

    Add products and cart constants

---
+++ src/app/constants/index.js
@@ -0,0 +1,6 @@
+// Product constants
+export const SET_PRODUCTS = 'SET_PRODUCTS';
+
+// Carts contants
+export const ADD_PRODUCT_CART = 'ADD_PRODUCT_CART';
+export const REMOVE_PRODUCT_CART = 'REMOVE_PRODUCT_CART';
