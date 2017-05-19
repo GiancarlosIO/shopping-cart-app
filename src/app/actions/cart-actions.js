08558c92418a74a795d9bee24da6075951321e3e
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-05-19 11:42:51 -0500

    Add cart-action to update total and subtotal

--- src/app/actions/cart-actions.js
+++ src/app/actions/cart-actions.js
@@ -1,9 +1,11 @@
 import {
   ADD_PRODUCT_CART,
   UPDATE_QUANTITY_CAR,
-  REMOVE_PRODUCT_CART
+  REMOVE_PRODUCT_CART,
+  UPDATE_TOTAL_CART
 } from '../constants/';
 
 export const addProductCart = (product) => ({ type: ADD_PRODUCT_CART, payload: product });
 export const removeProductCart = (id) => ({ type: REMOVE_PRODUCT_CART, payload: id });
 export const updateQuantityCart = (id, quantity) => ({ type: UPDATE_QUANTITY_CAR, payload: {id, quantity} });
+export const updateTotalCart = () => ({ type: UPDATE_TOTAL_CART });
