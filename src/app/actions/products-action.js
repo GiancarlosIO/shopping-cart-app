94451df73f24a74da31a2e100b4bf6a4857152f2
Giancarlos Isasi <giancarlos.isasi@gmail.com>
2017-05-19 01:20:16 -0500

    Add product actions to get all products and set searchText

--- src/app/actions/products-action.js
+++ src/app/actions/products-action.js
@@ -1,8 +1,10 @@
 import {
-  SET_PRODUCTS
+  SET_PRODUCTS,
+  SET_SEARCH_TEXT
 } from '../constants/';
 
 export const setProducts = (products) => ({ type: SET_PRODUCTS, payload: products });
+export const setSearchText = (text) => ({ type: SET_SEARCH_TEXT, payload: text });
 
 // Async actions;
 
@@ -10,7 +12,7 @@ export const getProductsByPage = (page) => {
   return (dispatch, getState, { ProductAPI }) => {
     return ProductAPI.getByPage(page).request
       .then(response => {
-        console.log('products', response);
+        // console.log('products', response);
         const productsObj = {};
         response.data.forEach(p => productsObj[`${p.id}`] = p );
         dispatch(setProducts(productsObj));
@@ -19,4 +21,19 @@ export const getProductsByPage = (page) => {
         console.log('error to get products', error);
       })
   }
-}
+};
+
+export const getAllProducts = () => {
+  return (dispatch, getState, { ProductAPI }) => {
+    return ProductAPI.getAll().request
+      .then(response => {
+        // console.log('products', response);
+        const productsObj = {};
+        response.data.forEach(p => productsObj[`${p.id}`] = p );
+        dispatch(setProducts(productsObj));
+      })
+      .catch(error => {
+        console.log('error to get products', error);
+      })
+  }
+};
